c840b8724a3cd1168d68c926d0ed53bcc58b660a
Janith <janitholagama.info@gmail.com>
2020-03-31 07:19:48 +0400

    Updates

---
+++ precache-manifest.ab096e524f2af507556d43bf17be54f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c8be88de67a3cd826cfeefe822145bb",
+    "url": "/monsters-rolodex-react_basics/index.html"
+  },
+  {
+    "revision": "45e3f3beaaef1a15039c",
+    "url": "/monsters-rolodex-react_basics/static/css/main.6dc66cf8.chunk.css"
+  },
+  {
+    "revision": "01e0a43a656d35760870",
+    "url": "/monsters-rolodex-react_basics/static/js/2.cbbe5488.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-react_basics/static/js/2.cbbe5488.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45e3f3beaaef1a15039c",
+    "url": "/monsters-rolodex-react_basics/static/js/main.979f1bdf.chunk.js"
+  },
+  {
+    "revision": "2af557174ddbbbf72e46",
+    "url": "/monsters-rolodex-react_basics/static/js/runtime-main.147055c9.js"
+  }
+]);
